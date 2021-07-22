4b4b05d9e0934baaae765ef5fc0a00c937ef768a
Jaqueline Silva <jaqueline.natario@gmail.com>
2021-07-22 17:48:08 -0300

    Resolve o requisito 8

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -20,11 +20,12 @@ class MovieCard extends React.Component {
 
 class MovieCard extends React.Component {
   render() {
-    const { movie } = this.props;
+    const { movie } = this.props; // linha 10 MovieList
     const { title, subtitle, storyLine, imagePath, rating } = movie;
     return (
       <section>
         <img src={ imagePath } alt={ title } />
+        <h4>{ title }</h4>
       </section>
 >>>>>>> 733ccde (Resolve o requisito 4)
     );
