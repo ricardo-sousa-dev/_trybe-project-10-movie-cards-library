7ccfcc02e5fb8be0cb81c355925b7bb49366796e
ricardo-sousa <rwmsousa@gmail.com>
2021-10-19 19:27:54 -0300

    resolvendo conflitos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,32 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 251b8ae (Resolve os requisitos 1 e 2)
 import React from 'react';
 
 class Header extends React.Component {
   render() {
     return (
+<<<<<<< HEAD
       <header className="movie-card-header">
         <h1 className="page-title">Movie Cards Library</h1>
+=======
+      <header>
+        <h1>Movie Cards Library</h1>
+>>>>>>> 251b8ae (Resolve os requisitos 1 e 2)
       </header>
     );
   }
 }
 
 export default Header;
+<<<<<<< HEAD
 =======
 // implement Header component here
 // first project in React
 >>>>>>> ce49938 (Iniciando o projeto.)
 =======
 >>>>>>> d473a51 (Resolve conflitos)
+=======
+>>>>>>> 251b8ae (Resolve os requisitos 1 e 2)
